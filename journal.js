aad7fabeaa5f7dd10d980d3f1e14de8f5bb13e1c
Shahab Yarmohammadi <yarmohammadi@gmail.com>
2025-07-03 11:39:26 -0700

    Add 4 new funcs: isEven(), square(), max(), celsiusToFarenheit()

--- journal.js
+++ journal.js
@@ -2,3 +2,23 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+
+// Check if a number is even
+function isEven(num) {
+  return num % 2 === 0;
+}
+
+// Compute square of a number
+function square(num) {
+  return num * num;
+}
+
+// Find larger of two numbers
+function max(a, b) {
+  return a > b ? a : b;
+}
+
+// Convert Celsius to Fahrenheit
+function celsiusToFahrenheit(celsius) {
+  return (celsius * 9/5) + 32;
+}
